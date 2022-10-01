d77091d385b1a43da135f15129d816c2ba7914e6
Ákos Barabás <akos.barabas2002@gmail.com>
2022-10-01 15:21:28 +0200

    added python quicksort

---
+++ quicksort.py
@@ -0,0 +1,31 @@
+def partition(array, low, high):
+    pivot = array[high]
+
+    i = low - 1
+ 
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+ 
+            (array[i], array[j]) = (array[j], array[i])
+ 
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+    return i + 1
+ 
+ 
+def quickSort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+
+        quickSort(array, low, pi - 1)
+        quickSort(array, pi + 1, high)
+ 
+ 
+data = [1123, 7, -123, 0, 1210, 9, -2, 1321, 0, 1, -1]
+print(f"Unsorted Array: {data}")
+ 
+size = len(data)
+quickSort(data, 0, size - 1)
+ 
+print(f"Sorted Array in Ascending Order: {data}")
